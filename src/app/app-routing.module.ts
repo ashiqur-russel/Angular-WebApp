0894013f0c0d2c5343e04945ab7a9afc63a4a1c8
ashiqurras <ashiqurrussel2@gmail.com>
2022-04-27 22:35:20 +0200

    Routing and event component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
+import { EventListComponent } from './components/event-list/event-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/card', pathMatch: 'full' },
+  { path: 'event', component: EventListComponent },
+  { path: '**', component: CardComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { EventComponent } from './components/event/event.component';
+import { EventListComponent } from './components/event-list/event-list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    EventComponent,
+    EventListComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
