3099b4713c5f66cb4d81cdee775e048081ac4e8a
Mohammad Ashiqur Rahman <28604009+ashiqur-russel@users.noreply.github.com>
2022-04-29 10:21:54 +0200

    Update event-details.component.ts

--- src/app/components/event-details/event-details.component.ts
+++ src/app/components/event-details/event-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { EventService } from 'src/app/services/event.service';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-event-details',
@@ -10,10 +12,10 @@ export class EventDetailsComponent implements OnInit {
 
   @Input() event: any;
 
-  constructor(private eventService: EventService) { }
+  constructor(private eventService: EventService,private route:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.event = this.eventService.getEvent(1)
+    this.events = this.eventService.getEvent(+this.route.snapshot.params['id']); // give parameters of cuurent route
 
   }
 
