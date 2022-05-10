4cc6c466f3e0d4320bec8e43e03824fc510d3558
ashiqurras <ashiqurrussel2@gmail.com>
2022-05-10 23:51:07 +0200

    login page and route child

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,7 @@
+import { ProfileComponent } from './profile/profile.component';
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { ProfileComponent } from "./profile.component"
 import { userRoutes } from "./user.routes";
 import { LoginComponent } from './login/login.component'
 
